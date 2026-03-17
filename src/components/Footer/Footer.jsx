492542b91f1a7bd41ca0770271c2e48d857a9e8e
elnararizv <rizvanovaelnara15@gmail.com>
2026-03-18 02:00:26 +0400

    add responsive footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import styles from "./Footer.module.css";
+import logo from "../../assets/image/logo.png";
+import { FaInstagram, FaFacebookF, FaTwitter, FaEnvelope, FaPhone } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.section}>
+          <img src={logo} alt="Velvetta" className={styles.logo} />
+          <p className={styles.description}>
+            Velvetta is a modern patisserie offering fresh,
+            handcrafted desserts made with love and quality ingredients.
+          </p>
+        </div>
+
+        <div className={styles.section}>
+          <h3>Contact</h3>
+          <p><FaPhone /> +994 50 123 45 67</p>
+          <p><FaEnvelope /> info@velvetta.com</p>
+        </div>
+
+        <div className={styles.section}>
+          <h3>Follow Us</h3>
+          <div className={styles.social}>
+            <a href="#"><FaInstagram /></a>
+            <a href="#"><FaFacebookF /></a>
+            <a href="#"><FaTwitter /></a>
+          </div>
+        </div>
+
+      </div>
+
+      <div className={styles.bottom}>
+        © {new Date().getFullYear()} Velvetta. All rights reserved.
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
